0b92ebe490666c785c12b7e735bf6aaf14aa5983
razielmtz <razielmartinezcastillo@gmail.com>
2020-09-14 17:24:40 -0500

    Chatbot with html parser

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
-//React Components
 import React from "react";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Widget, addResponseMessage, renderCustomComponent} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import ReactHtmlParser from 'react-html-parser';
 import Home from "./Pages/Home";
 import LandingPage from "./Pages/LandingPage";
 import User from "./Pages/User";
 import "./App.css";
-import * as axios from 'axios';
+import "./Components/ChatBot.js";
+import Chat from "./Components/ChatBot.js";
 
-const handleNewUserMessage = async (message) => {
-  const { data } = await axios.post('http://127.0.0.1:5002/getMessage', {
-    message
-  });
 
-  addResponseMessage(data.text);
-};
 
-/* Checa esto raziel xD*/
-/*----------------------------------------------------------------------------------------------
-jaja le cree el componente en el components*/
-/*class Translate extends React.Component{
-  render(){
-    return (
-      <div className="speech-bubble">
-      {ReactHtmlParser(this.props.text)}
-      </div>
-    );
-  }
-}
-class Chat extends React.Component{
-  handleNewUserMessage=(newMessage)=>{
-    this.handleSubmit(newMessage)
-  };
-  handleSubmit=(message)=>{
-    axios.post("http://127.0.0.1:5002/getMessage", { message }).then((res)=>{
-      console.log(res.data);
-      renderCustomComponent(Translate, {text: res.data.text});
-      return res.data;
-    });
-  };
-}
-*/
+
 export default function App() {
   return (
     <Router>
       <NavBar />
-      <Widget
-        handleNewUserMessage={handleNewUserMessage}
-        title="CHATBOT"
-        subtitle="Ask me"
-      />
+
+      <Chat/>
+     
       <Switch>
         <Route exact path="/">
           <LandingPage />
---
+++ src/Components/ChatBot.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Widget, renderCustomComponent} from "react-chat-widget";
+import 'react-chat-elements/dist/main.css';
+import axios from "axios";
+import ReactHtmlParser from 'react-html-parser';
+import "./../App.css";
+
+class Translate extends React.Component{
+  render(){
+    return (
+      <div className="burbujita">
+      {ReactHtmlParser(this.props.text)}
+      </div>);
+      }}
+
+class Chat extends React.Component{
+    handleNewUserMessage=(newMessage)=>{
+    this.handleSubmit(newMessage)};
+
+  handleSubmit=(message)=>{
+    axios.post("http://127.0.0.1:5002/getMessage", { 
+      message 
+    }).then((res)=>{
+      console.log(res.data);
+      renderCustomComponent(Translate, {text: res.data.text});
+    });
+  };
+  render(){
+    return(
+      <Widget
+        handleNewUserMessage={this.handleNewUserMessage}
+        title="CHATBOT"
+        subtitle="Ask me"
+      />
+    )
+  }
+}
+export default Chat;
